0ad6a8b9250b50e7056b3ab00ecfb2859f2f3c25
rewine <luhongxu@deepin.org>
2023-03-30 15:17:42 +0800

    qt6.qtdoc: fix build

--- pkgs/development/libraries/qt-6/modules/qtdoc.nix
+++ pkgs/development/libraries/qt-6/modules/qtdoc.nix
@@ -1,9 +1,27 @@
 { qtModule
 , qtdeclarative
+, qtbase
+, qttools
 }:
 
 qtModule {
   pname = "qtdoc";
+  # avoid fix-qt-builtin-paths hook substitute QT_INSTALL_DOCS to qtdoc's path
+  prePatch = ''
+    for file in $(grep -rl '$QT_INSTALL_DOCS'); do
+      echo "fixQtDocPaths: Fixing Qt Doc paths in $file..."
+      substituteInPlace $file \
+          --replace '$QT_INSTALL_DOCS' "${qtbase}/share/doc"
+    done
+  '';
+  nativeBuildInputs = [ qttools ];
   qtInputs = [ qtdeclarative ];
+  cmakeFlags = [
+    "-DCMAKE_MESSAGE_LOG_LEVEL=STATUS"
+  ];
+  dontUseNinjaBuild = true;
+  buildFlags = [ "docs" ];
+  dontUseNinjaInstall = true;
+  installFlags = [ "install_docs" ];
   outputs = [ "out" ];
 }
