b327b95e6fae9237facc8b0a59f1a415d6e97db4
rewine <luhongxu@deepin.org>
2023-03-30 15:06:11 +0800

    qt6.qtbase: allow find qt tools in QTTOOLSPATH

--- pkgs/development/libraries/qt-6/default.nix
+++ pkgs/development/libraries/qt-6/default.nix
@@ -60,6 +60,7 @@ let
           ./patches/qtbase-qmake-pkg-config.patch
           ./patches/qtbase-tzdir.patch
           ./patches/qtbase-variable-fonts.patch
+          ./patches/qtbase-find-tools-in-QTTOOLSPATH.patch
           # Remove symlink check causing build to bail out and fail.
           # https://gitlab.kitware.com/cmake/cmake/-/issues/23251
           (fetchpatch {
