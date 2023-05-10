cbb8f71e02fe51286673263a7ef462351f02856d
rewine <lhongxu@outlook.com>
2023-05-10 14:40:14 +0800

    calamares-nixos-extensions: add deepin

--- pkgs/tools/misc/calamares-nixos-extensions/default.nix
+++ pkgs/tools/misc/calamares-nixos-extensions/default.nix
@@ -5,10 +5,10 @@ stdenv.mkDerivation rec {
   version = "0.3.11";
 
   src = fetchFromGitHub {
-    owner = "NixOS";
+    owner = "wineee";
     repo = "calamares-nixos-extensions";
-    rev = version;
-    sha256 = "NAHUU0tQLu8c2dke1V0aM3mHrNgM8ekHSB2Fo9dQUk8=";
+    rev = "50de085488ac6784540b8c90c30480a7feff05f3";
+    sha256 = "sha256-zaswgXZXaM4Up14kQoTKZ7Gww5O0pNVDWG8j4Sv8mvo=";
   };
 
   installPhase = ''
